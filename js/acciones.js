3d0fe23a82209592cc16a8660cd5f9d4735354f9
campos92 <vanessa3f.dsaupoo@gmail.com>
2016-05-31 21:27:22 -0500

    2subida

---
+++ js/acciones.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+var audio;
+$(document).ready(function(e) {
+	document.addEventListener("deviceready", function(){
+		audio = window.plugins.LowLatencyAudio;
+		audio.preloadFX('do', 'audio/DO.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('re', 'audio/RE.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('mi', 'audio/MI.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('fa', 'audio/FA.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('sol', 'audio/SOL.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('la', 'audio/LA.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('si', 'audio/SI.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		
+		audio.preloadFX('do2', 'audio/DO#.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('re2', 'audio/RE#.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('fa2', 'audio/FA#.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('sol2', 'audio/SOL#.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('la2', 'audio/LA#.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		
+		audio.preloadFX('do3', 'audio/DO#.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('re3', 'audio/RE#.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('fa3', 'audio/FA#.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('sol3', 'audio/SOL#.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		audio.preloadFX('la3', 'audio/LA#.mp3', function(msg){}, function(msg){alert ('Error: ' + msg);});
+		
+		$('.nota').bind('touchstart', function(){
+			$(this).addClass('tocada');
+			audio.play($(this).attr('id'));
+		}).bind('touchend', function(){
+			$(this).removeClass('tocada');
+		});
+		$('.nota1').bind('touchstart', function(){
+			$(this).addClass('tocada');
+			audio.play($(this).attr('id'));
+		}).bind('touchend', function(){
+			$(this).removeClass('tocada');
+		});
+	});
+});
